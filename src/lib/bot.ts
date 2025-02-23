4031b9f071b1a037e99bfc48c659eb6ad68dba23
Olga Golets <olga.golets@air-up.com>
2025-02-23 20:41:05 +0100

    respond to event

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,14 +1,28 @@
 import TelegramBot from 'npm:node-telegram-bot-api';
+import { match } from 'npm:ts-pattern';
 import { saveMessage } from './botHistory.ts';
-//import 'dotenv/config';
+import 'dotenv/config';
 
 export const bot = new TelegramBot(Deno.env.get('BOT_TOKEN'));
 
 bot.on('message', async (message: TelegramBot.Message) => {
 	try {
-		await bot.sendMessage(message.chat.id, 'Roger');
+		await match(message.text)
+			.with('\/event', async () => await createNewEvent(bot, message))
+			.otherwise(async () => await bot.sendMessage(message.chat.id, 'Roger'));
 		saveMessage({ loggedAt: new Date(), message });
 	} catch (e) {
 		console.error(e);
 	}
 });
+
+const createNewEvent = async (bot: TelegramBot, message: TelegramBot.Message) => {
+	const options = {
+		reply_to_message_id: message.message.id,
+		reply_markup: {
+			resize_keyboard: true,
+			keyboard: [[{ text: 'Click here!' }]]
+		}
+	};
+	await bot.sendMessage(message.chat.id, 'Let me help to create you a new event', options);
+};
