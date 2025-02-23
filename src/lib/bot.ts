b38abf93e0cf2ba060d586b752e47474513ee68e
Olga Golets <olga.golets@air-up.com>
2025-02-23 21:17:36 +0100

    add a basic keyboard reply

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -2,14 +2,26 @@ import TelegramBot from 'npm:node-telegram-bot-api';
 import { match } from 'npm:ts-pattern';
 import { saveMessage } from './botHistory.ts';
 //import 'dotenv/config';
+import { logger } from '../logger.ts';
 
 export const bot = new TelegramBot(Deno.env.get('BOT_TOKEN'));
 
 bot.on('message', async (message: TelegramBot.Message) => {
 	try {
 		await match(message.text)
-			.with('\/event', async () => await createNewEvent(bot, message))
-			.otherwise(async () => await bot.sendMessage(message.chat.id, 'Roger'));
+			.with(
+				'\/event',
+				async () =>
+					await createNewEvent(bot, message).then((reply) =>
+						logger.debug('Reply: {reply}', { reply })
+					)
+			)
+			.otherwise(
+				async () =>
+					await logger.debug('Logging not fitting requests: {message}', {
+						message: JSON.stringify(message)
+					})
+			);
 		saveMessage({ loggedAt: new Date(), message });
 	} catch (e) {
 		console.error(e);
@@ -18,10 +30,11 @@ bot.on('message', async (message: TelegramBot.Message) => {
 
 const createNewEvent = async (bot: TelegramBot, message: TelegramBot.Message) => {
 	const options = {
-		reply_to_message_id: message.message.id,
+		reply_to_message_id: message.message_id,
 		reply_markup: {
 			resize_keyboard: true,
-			keyboard: [[{ text: 'Click here!' }]]
+			one_time_keyboard: true,
+			keyboard: [['Click here!'], ['Click there!']]
 		}
 	};
 	await bot.sendMessage(message.chat.id, 'Let me help to create you a new event', options);
